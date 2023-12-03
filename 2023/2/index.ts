a205d66e0319c53db0dae7bf55b8a5bbf700ea4d
Eduardo Alvarez <eduardo.alvarez@protonmail.com>
2023-12-02 19:49:53 -0500

    feat: include bun as runtime for resolution

---
+++ 2023/2/index.ts
@@ -0,0 +1,36 @@
+const path = import.meta.dir + "/input.txt";
+
+const file = Bun.file(path);
+
+const text = await file.text();
+
+const colors: Record<string, number> = {
+  red: 12,
+  green: 13,
+  blue: 14,
+};
+
+const r = text
+  .trim()
+  .split("\n")
+  .reduce((acc, line) => {
+    const regex = /Game (\d+):(.*)/g;
+    const match = regex.exec(line);
+    if (!match) {
+      return acc;
+    }
+
+    const [, game, players] = match;
+    const playersTrim = players.trim();
+    const re = /(\d+) (red|green|blue)/g;
+    let m;
+    while ((m = re.exec(playersTrim)) !== null) {
+      const [, points, color] = m;
+      if (colors[color] < Number(points)) {
+        return acc;
+      }
+    }
+
+    return acc + Number(game);
+  }, 0);
+console.log(r);
