dec1aaa52f7cb2a759f26bc21bc9482ae5f9036a
Eduardo Alvarez <eduardo.alvarez@protonmail.com>
2023-12-04 17:57:01 -0500

    feat: part two day four

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -17,18 +17,40 @@ def part_one():
         card = int(card)
         first = [int(x) for x in first.split()]
         second = [int(x) for x in second.split()]
-        a = 0
-        for i in first:
-            if i in second:
-                a += 1
+        common = set(first) & set(second)
+        a = len(common)
         if a == 0:
             continue
         total += 2 ** (a - 1)
     print(total)
 
 
+def part_two():
+    lines = read_file("input.txt")
+    carry = [1] * len(lines)
+    for k, line in enumerate(lines):
+        found = re.findall(r"Card\s+(\d+):\s+([\d\s]+) \| ([\d\s]+)", line)
+        card, first, second = found[0]
+        card = int(card)
+        first = [int(x) for x in first.split()]
+        second = [int(x) for x in second.split()]
+        common = set(first) & set(second)
+        a = len(common)
+        cards = carry[k]
+        if a == 0:
+            continue
+
+        for j in range(1, a + 1):
+            if (j + k) > len(lines):
+                break
+            carry[j + k] += cards
+
+    print(sum(carry))
+
+
 def main():
     part_one()
+    part_two()
 
 
 if __name__ == "__main__":
